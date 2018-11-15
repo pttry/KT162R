8d5de7df519ac7cbe8963abad956a848659e26af
Janne Huovari <janne.huovari@ptt.fi>
2018-11-15 14:13:38 +0200

    Fig pend_atyyppi atyyppi classification change atyyppi_colours

--- data-raw/luokitukset.R
+++ data-raw/luokitukset.R
@@ -13,8 +13,13 @@ aluetyyppi <- readxl::read_xls("data-raw/msindaluejaot10.xls") %>%
          aluetyyppi = coalesce(luokka2, Alueluokka)) %>%
   select(kunta_no, kunta, aluetyyppi) %>%
   mutate(aluetyyppi = fct_relevel(aluetyyppi,
-                                  "Harvaan asuttu maaseutu", "Ydinmaaseutu", "Kaupunkien läh. maaseutu",
-                                  "Kaupungit", "TKA", "YO", "PK seutu"))
+                                  rev(c("Harvaan asuttu maaseutu", "Ydinmaaseutu", "Kaupunkien läh. maaseutu",
+                                  "Kaupungit", "TKA", "YO", "PK seutu")))) %>%
+  mutate(aluetyyppi = fct_recode(aluetyyppi,
+                                 'Muut kaupungit' = "Kaupungit",
+                                 'Muut TKA keskukset' = "TKA",
+                                 'Muut yliopistokaupungit' = "YO",
+                                 'PK-seutu' = "PK seutu"))
 
 
 use_data(aluetyyppi, overwrite = TRUE)
