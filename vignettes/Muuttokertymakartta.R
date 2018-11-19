f48f44a5d82dee0aaec7178ea45a123613d8ff03
Jalasalmi <juho.alasalmi@ptt.fi>
2018-11-19 19:31:35 +0100

    Add codes to plots to the vignettes not that .R files are not ignored anymore

---
+++ vignettes/Muuttokertymakartta.R
@@ -0,0 +1,141 @@
+# Muuttokertymäkartta
+
+# Hae ja muokkaa data
+dat_muuttotiedot_kunnittain <- readRDS("data/dat_muuttotiedot_kunnittain.rds")
+reg_keytable <- readRDS("data/reg_keytable.rds")
+kuntakoodit <- select(reg_keytable, Knro, Kunta) %>%
+  mutate(Kunta = as.character(Kunta))
+df_kunnat <- readRDS("data/spdf.rds")
+
+# Jotain menee pieleen kuntien yhdistämisessä, tekee valkoisia läikkiä karttaan.
+
+# df_kunnat$NATCODE <- as.double(as.integer(df_kunnat$NATCODE))
+#  kuntakoodit$Knro <- as.double(as.integer(kuntakoodit$Knro))
+#
+# # Yhdistä Luvia Eurajokeen ja Juankoski Kuopioon, muuttamalla Luvian ja Juankosken kuntakoodit
+# df_kunnat$NATCODE <- fct_collapse(df_kunnat$NATCODE, "297" = c("174", "297"))
+# df_kunnat$NATCODE <- fct_collapse(df_kunnat$NATCODE, "051" = c("442", "051"))
+# kuntakoodit$Knro <- fct_collapse(kuntakoodit$Knro, "297" = c("174", "297"))
+# kuntakoodit$Knro <- fct_collapse(kuntakoodit$Knro, "051" = c("442", "051"))
+# kuntakoodit$Kunta[kuntakoodit$Kunta == "Juankoski"] <- "Kuopio"
+# kuntakoodit$Kunta[kuntakoodit$Kunta == "Luvia"] <- "Eurajoki"
+
+# Yhdistä luvia ja juankoski lisäämällä ne dataan
+
+Juankoski <- dat_muuttotiedot_kunnittain %>% filter(Alue == "Kuopio")
+Juankoski$Alue <- "Juankoski"
+Luvia <- dat_muuttotiedot_kunnittain %>% filter(Alue == "Eurajoki")
+Luvia$Alue <- "Luvia"
+
+dat_muuttotiedot_kunnittain <- rbind(dat_muuttotiedot_kunnittain, Juankoski, Luvia)
+
+
+df <- dat_muuttotiedot_kunnittain %>%
+  filter(Vuosi %in% 1990:2017) %>%
+  group_by(Alue) %>%
+  filter(Tiedot == "nettomuutto") %>%
+  spread(Tiedot, values) %>%
+  summarise(muutos = sum(nettomuutto)/mean(vakiluku)) %>%
+  ungroup() %>%
+  mutate(Kunta = Alue) %>%
+  select(Kunta, muutos) %>%
+  right_join(rename(kuntakoodit, NATCODE = "Knro"), by = "Kunta") %>%
+  left_join(distinct(df_kunnat, id, NATCODE), by = "NATCODE")
+
+# Piirrä kartta
+ggplot(
+  df,
+  aes(
+    fill = muutos
+  )
+) +
+  geom_map(aes(map_id = id), map = df_kunnat) +
+  coord_fixed(0.9) +
+  xlim(0, 8e+05) +
+  ylim(6600000, 7800000) +
+  scale_fill_gradient2(
+    high = "red4",
+    mid = "white",
+    midpoint = 0,
+    low = "darkgreen",
+    name = "Muutos"
+  ) +
+  scale_size_manual(values = c(0.2, 1), guide = "none") +
+  scale_colour_manual(values = c("grey80", "grey20"), guide = "none") +
+  theme(
+    axis.text = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
+    axis.title = element_blank(),
+    panel.grid.major = element_blank()
+  ) +
+  labs(caption = "Lähde: Työnvälitystilasto (TEM), Tilastokeskus, PTT")
+
+
+# The geom_polygon-cowplot way
+
+df <- dat_muuttotiedot_kunnittain %>%
+  filter(Vuosi %in% 2008:2017) %>%
+  group_by(Alue) %>%
+  filter(Tiedot == "nettomuutto") %>%
+  spread(Tiedot, values) %>%
+  summarise(muutos = sum(nettomuutto)) %>%
+  ungroup() %>%
+  mutate(Kunta = Alue) %>%
+  select(Kunta, muutos) %>%
+  right_join(rename(kuntakoodit, NATCODE = "Knro"), by = "Kunta") %>%
+  left_join(distinct(df_kunnat, id, NATCODE), by = "NATCODE")
+
+unset_ptt()
+library(cowplot)
+df <- df %>% left_join(df_kunnat, df, by = "NATCODE")
+
+df_muuttovoitolliset <- filter(df, muutos >= 0)
+df_muuttotappiolliset <- filter(df, muutos < 0)
+
+# Tiedosto "nettomuuttokertym?karttadata.Rda" luotu scriptiss? "Muuttokertym?karttadatan luonti"
+# kansiossas "Kuvaajien R koodit/Datan siistint?"
+
+muuttovoitto <- ggplot() + geom_polygon(data = df_muuttovoitolliset,
+                                        aes(x = long, y = lat,fill = muutos, group = group)) +
+  scale_fill_gradientn(colours = colorRampPalette(c("darkgreen", "lawngreen"))(100),
+                       name = "Muuttovoitto") +
+  coord_fixed(0.9) +
+  xlim(0, 8e+05) +
+  ylim(6600000,7800000) +
+  theme(axis.text = element_blank(),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        panel.grid.major = element_blank(),
+        legend.position = c(0.97,0.6)) +
+  labs(caption = "Lähde: PTT, Tilastokeskus",
+       title = "Nettomuuttokertymä kunnittain 2008-2017")
+
+muuttotappio <- ggplot() + geom_polygon(data = df_muuttotappiolliset,
+                                        aes(x = long, y = lat,fill = muutos, group = group)) +
+  scale_fill_gradientn(colours = colorRampPalette(c("red4", "lightcoral"))(100),
+                       name = "Muuttotappio") +
+  coord_fixed(0.9) +
+  xlim(0, 8e+05) +
+  ylim(6600000,7800000) +
+  theme(axis.text = element_blank(),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        panel.grid.major = element_blank(),
+        legend.justification = "bottom",
+        legend.position = c(1.14,0.20)) +
+  labs(caption = "Lähde: PTT, Tilastokeskus",
+       title = "Nettomuuttokertymä kunnittain 2008-2017")
+# Yhdist? muuttotappio- ja muuttovoittokartat
+
+ggdraw() + draw_plot(muuttotappio, x = -0.1, y = 0) +
+  draw_plot(muuttovoitto, x = -0.1, y = 0)
+
---
+++ vignettes/Muuttotyollisyyskuva.R
@@ -0,0 +1,96 @@
+library(pxweb)
+library(statfitools)
+library(ggptt)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(cowplot)
+library(grid)
+
+data0 <-
+  get_pxweb_data(url = "http://pxnet2.stat.fi/PXWeb/api/v1/fi/StatFin/vrm/vamuu/statfin_vamuu_pxt_004.px",
+                 dims = list(Kuukausi = c('*'),
+                             Tiedot = c('04')),
+                 clean = TRUE)
+
+data <- data0 %>% mutate(Vuosi = substring(Kuukausi, 0,4)) %>%
+  mutate(Kuukausi = substring(Kuukausi, 6,7)) %>%
+  mutate(time = as.Date(paste(Vuosi, Kuukausi, "01", sep="-"))) %>%
+  select(values, time) %>%
+  mutate(values_sa = sa_series(values, time),
+         values_trend = trend_series(values, time)) %>%
+  gather(value_type, value, c("values", "values_sa", "values_trend"))
+
+data %>% filter(time >= as.Date("2000-01-01")) %>%
+  ggplot(aes(x = time, y = value, col = value_type)) +
+  geom_line() +
+  theme_ptt() +
+  scale_color_discrete(labels = c("Kuntien väliset muutot",
+                                  "Kuntien väliset muutot, sa",
+                                  "Kuntien väliset muutot, trend")) +
+  ylab("Kuntien välisiä muuttoja") +
+  xlab(NULL) +
+  theme(legend.position = "bottom", legend.justification = "left") +
+  theme(legend.title = element_blank()) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  theme(axis.text.x=element_text(angle=60, hjust=1)) +
+  ggtitle("Kuntien väliset muutot kuukausittain")
+
+tvt <-
+  get_pxweb_data(url = "http://pxnet2.stat.fi/PXWeb/api/v1/fi/StatFin/tym/tyti/statfin_tyti_pxt_005.px",
+                 dims = list(Tiedot = c('Tyolliset', 'Tyotunnit'),
+                             Toimiala = c('SS'),
+                             Vuosi = c('*'),
+                             Ajanjakso = c('*')),
+                 clean = TRUE)
+
+tvt <- clean_times(tvt, sub_year_col = "Ajanjakso", agg_time = "Vuosikeskiarvo") %>%
+  mutate(value_type = ifelse(grepl("työtunnit", Tiedot), "työtunnit", "työllisten_määrä")) %>%
+  select(-Tiedot, -Toimiala) %>%
+  mutate(values = 1000*values) %>%
+  spread(value_type, values) %>%
+  mutate(työllisten_määrä_trend = trend_series(työllisten_määrä, time),
+         työtunnit_trend = trend_series(työtunnit, time)) %>%
+  gather(value_type, values, 2:5)
+
+
+
+p1 <- tvt %>%  filter(time >= as.Date("2005-01-01"),
+                      value_type == "työllisten_määrä_trend") %>%
+  ggplot(aes(x = time, y = values/1000000)) +
+  geom_line() +
+  theme_ptt() +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_x_date(limits = c(as.Date("2005-01-01"), as.Date("2018-07-01")),
+               date_breaks = "1 year", labels = no_century) +
+  ggtitle("Työllisten määrä, milj., trendi") +
+  theme(axis.text.x = element_blank()) +
+  scale_y_continuous(labels = deci_comma)
+
+p2 <- tvt %>%  filter(time >= as.Date("2005-01-01"),
+                      value_type == "työtunnit_trend") %>%
+  ggplot(aes(x = time, y = values/1000000)) +
+  geom_line() +
+  theme_ptt() +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_x_date(limits = c(as.Date("2005-01-01"), as.Date("2018-07-01")),
+               date_breaks = "1 year", labels = no_century) +
+  theme(axis.text.x = element_blank()) +
+  ggtitle("Työtunnit, milj., trendi")
+
+p3 <- data %>% filter(time >= as.Date("2005-01-01"),
+                      value_type == "values_trend") %>%
+  ggplot(aes(x = time, y = value)) +
+  geom_line() +
+  theme_ptt() +
+  xlab(NULL) +
+  ylab(NULL) +
+  scale_x_date(limits = c(as.Date("2005-01-01"), as.Date("2018-07-01")),
+               date_breaks = "1 year", labels = no_century) +
+  labs(title = "Kuntien välinen muuttoliike, muuttoja kuukaudessa, trendi",
+       caption = "Lähde: Tilastokeskus, PTT")
+
+grid.newpage()
+grid.draw(rbind(ggplotGrob(p1), ggplotGrob(p2), ggplotGrob(p3), size = "last"))
