7fc9fa8e6f8bd924c218154d86741e4c0d7a40a4
Jalasalmi <juho.alasalmi@ptt.fi>
2018-11-19 22:12:08 +0100

    Update plots of Kirjallisuuskatsaus

--- vignettes/plots.R
+++ vignettes/plots.R
@@ -55,7 +55,7 @@ ggsave("vignettes/muuttoasteet90_17.png")
 
 ###### Plot pendelelöijien absoluuttinen määrä koko maassa ################################
 
-pendelointi_data <- readRDS("R/data_clean/pendelointi_data.rds")
+pendelointi_data <- readRDS("data/pendelointi_data.rds")
 # Data muokattu Tilastokeskuksen Datasta scriptissä "Pendelöintiaineistojen siistintä"
 # kansiossa Kuvaajien R koodit/Datan siistintä
 
@@ -65,6 +65,7 @@ pendelointi_data <- readRDS("R/data_clean/pendelointi_data.rds")
 pendelointi_data %>% group_by(vuosi) %>%
   summarize(commuters = sum(lahtopendelointi)) %>%
   ggplot(aes(y = commuters, x = vuosi)) +
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 6)) +
   geom_line() +
   #ggtitle("Pendelöijien määrä Suomessa 1987 - 2015") + #Kuntien välinen pendelöinti
   ylab("Pendelöijien määrä") +
@@ -79,8 +80,71 @@ pendelointi_data %>% group_by(vuosi) %>%
   mutate(pendelointiosuus = pendeloijat_yhteensa /
            (pendeloijat_yhteensa + ei_pendeloijat_yhteensa)) %>%
   ggplot(aes(y = pendelointiosuus, x = vuosi)) +
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 6)) +
   geom_line() +
   #ggtitle("Pendelöijien osuus työllisistä Suomessa 1987 - 2015") +
   ylab("Pendelöijien osuus työllisistä") +
   xlab(NULL) +
   scale_y_continuous(limits = c(0.19,0.35), labels = percent_comma)
+
+# Nettomuuttokertymä barplot seutukunnittain
+
+# Hae data
+  dat_muuttotiedot_kunnittain <- readRDS("data/dat_muuttotiedot_kunnittain.rds")
+  nettomuutto_data <- filter(dat_muuttotiedot_kunnittain, Tiedot == "nettomuutto")
+
+nettomuutto_data %>% group_by(seutukunta) %>%
+  summarize(nettomuutto = sum(values)) %>%
+  ggplot(aes(x = reorder(seutukunta, nettomuutto), y = nettomuutto)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  xlab(NULL) +
+  ylab("Nettomuutto") +
+  scale_y_continuous(limits = c(-50000, 140000), labels = deci_comma)
+
+####### KUNTIEN VÄLINEN PENDELÖINTI JA KUNTIEN VÄLINEN MUUTTO SUHTEESSA VÄKILUKUUN #############
+
+# Hae data
+load("data/dat_lahtopendelointi.rda")
+dat_lahtopendelointi <- dat_lahtopendelointi %>%
+                        mutate(Vuosi = as.integer(time)) %>%
+                        filter(alue == "KOKO MAA",
+                               koulutusaste == "Yhteensä",
+                               ika == "Kaikki ikäluokat",
+                               pendelointi == "Pendelöivät")
+muutot <- readRDS("data/muuttoaste_data.rds")
+vakiluku <- readRDS("data/vakiluku.rds")
+
+kuntien_valiset_muutot <- filter(muutot, muuton_tyyppi == "kuntien valiset muutot") %>%
+  rename(Vuosi = vuosi) %>%
+  filter(Vuosi %in% 1990:2016) %>%
+  select(Vuosi, muuttoja, vakiluku)
+
+  pendelointi_ja_muutto <- left_join(kuntien_valiset_muutot,
+                                   dat_lahtopendelointi,
+                                   by = "Vuosi")
+pendelointi_ja_muutto <- pendelointi_ja_muutto %>%
+  mutate(Kuntien_valinen_pendelointi = values / vakiluku) %>%
+  mutate(Kuntien_valinen_muutto = muuttoja / vakiluku) %>%
+  select(Vuosi, Kuntien_valinen_pendelointi, Kuntien_valinen_muutto) %>%
+  gather(tyyppi, values, c("Kuntien_valinen_pendelointi", "Kuntien_valinen_muutto"))
+
+pendelointi_ja_muutto %>% ggplot(aes(x = Vuosi, y = values, colour = tyyppi)) +
+  geom_line() +
+  ylab(NULL) +
+  xlab(NULL) +
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 6)) +
+  scale_y_continuous(labels = percent_comma) +
+  theme(legend.position = "bottom",
+        legend.justification = "left",
+        legend.title = element_blank()) +
+  scale_color_discrete(labels = c("Kuntien välinen muutto",
+                                  "Kuntien välinen pendelöinti"))
+
+ggsave(filename = "Kuntien välinen pendelöinti ja kuntien välinen muutto.jpeg",
+       path = path,
+       device = "jpeg",
+       width = 17,
+       height = 10,
+       units = "cm")
+
