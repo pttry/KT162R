0fbb5c9e0b728ced9fad2aa69d6e942cb41cfbe3
jalasalmi <juho.alasalmi@ptt.fi>
2019-01-28 13:15:03 +0100

    add vignette with sample selection simulations

--- R/llf_sartori.R
+++ R/llf_sartori.R
@@ -13,9 +13,9 @@ llf_sartori <- function(parameters) {
   selection_param <- parameters[1]
   outcome_param <- parameters[2]
 
-  P0 <- sum(log(pnorm(-selection_param * x))[!selected])
-  P1 <- sum(log((pnorm(-outcome_param * x) - pnorm(-selection_param * x))[selected & !yo][(selection_param - outcome_param)*xs[selected & !yo] > 0]))
-  P2 <- sum(log(sapply(lapply(lapply(1:sum(selected & yo), function(i) matrix(c(selection_param * x[selected & yo], outcome_param * x[selected & yo]), nrow = 2, byrow = TRUE)[,i]),
+  P0 <- sum(log(pnorm(-selection_param * x))[!ys])
+  P1 <- sum(log((pnorm(-outcome_param * x) - pnorm(-selection_param * x))[ys & !yo][(selection_param - outcome_param)*x[ys & !yo] > 0]))
+  P2 <- sum(log(sapply(lapply(lapply(1:sum(ys & yo), function(i) matrix(c(selection_param * x[ys & yo], outcome_param * x[ys & yo]), nrow = 2, byrow = TRUE)[,i]),
                               pnorm), min)))
   ll <- P0 + P1 + P2
   ll
