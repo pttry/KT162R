9d1119d1300e776d12abd7f9a788174c91bfb218
jalasalmi <juho.alasalmi@ptt.fi>
2019-01-28 16:26:07 +0100

    amend the function to be able to handle multiple regressors

--- R/llf_sartori.R
+++ R/llf_sartori.R
@@ -3,20 +3,27 @@
   # "x" is currently the single explanatory variable. In Sartori's estimator the explanatory variables are the same for
   # the selection and outcome equations.
 
-  # "Parameters" is a vector, where the first element is the coefficient in the selection equation and the second
-  # element is the coefficient in the outcome equation.
+  # "Parameters" is a vector, where the first k elements are the coefficients in the selection equation and the rest are
+  # the coefficients of the outcome equation
 
   # NOTE: rho does not enter the estimator since the estimator assumes it to be 1
 
 llf_sartori <- function(parameters) {
 
-  selection_param <- parameters[1]
-  outcome_param <- parameters[2]
+  selection_param <- as.matrix(parameters[1:k])
+  outcome_param <- as.matrix(parameters[(k+1):length(parameters)])
 
-  P0 <- sum(log(pnorm(-selection_param * x))[!ys])
-  P1 <- sum(log((pnorm(-outcome_param * x) - pnorm(-selection_param * x))[ys & !yo][(selection_param - outcome_param)*x[ys & !yo] > 0]))
-  P2 <- sum(log(sapply(lapply(lapply(1:sum(ys & yo), function(i) matrix(c(selection_param * x[ys & yo], outcome_param * x[ys & yo]), nrow = 2, byrow = TRUE)[,i]),
-                              pnorm), min)))
-  ll <- P0 + P1 + P2
+  m <- matrix(c(X[ys & yo,] %*% selection_param, X[ys & yo,] %*% outcome_param), nrow = 2, byrow = TRUE)
+
+  P0 <- pnorm(X %*% -selection_param)[!ys]
+  P1 <- (pnorm(X %*% -outcome_param) - pnorm(X %*% -selection_param))[ys & !yo][(X[ys & !yo,] %*% (selection_param - outcome_param)) > 0]
+  P2 <- sapply(
+          lapply(
+            lapply(1:sum(ys & yo), function(i) m[,i]), pnorm),
+          min)
+  ll <- sum(log(P0)) + sum(log(P1)) + sum(log(P2))
   ll
 }
+
+
+llf_sartori(c(0,1.25,-0.7, 1.5))
