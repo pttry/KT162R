c3eeadd9f402bfecc443bf35b4e26fc80fd6d263
Janne Huovari <janne.huovari@ptt.fi>
2019-10-14 13:00:11 +0300

    Fix muuuttotiedot kunnitta Maarianhamina

--- data-raw/dat_muuttotiedot_kunnittain_prep.R
+++ data-raw/dat_muuttotiedot_kunnittain_prep.R
@@ -2,56 +2,56 @@
 
 library(pxweb)
 library(tidyverse)
-source("R/kuntaluokka.R")
-
-# Get the data
-dat_muuttotiedot_kunnittain <-
-  get_pxweb_data(url = "http://pxnet2.stat.fi/PXWeb/api/v1/fi/StatFin/vrm/muutl/statfin_muutl_pxt_11a2.px",
-                 dims = list(Alue = c('*'),
-                             Vuosi = c('*'),
-                             Sukupuoli = c('SSS'),
-                             Ikä = c('SSS'),
-                             Tiedot = c('*')),
-                 clean = TRUE)
-
-# Change types of Alue and Vuosi and names in Tiedot
-   dat_muuttotiedot_kunnittain$Alue <- as.character(dat_muuttotiedot_kunnittain$Alue)
-   dat_muuttotiedot_kunnittain$Vuosi <- as.integer(as.character(dat_muuttotiedot_kunnittain$Vuosi))
-   levels(dat_muuttotiedot_kunnittain$Tiedot) <- c("tulomuutto", "lahtomuutto", "nettomuutto")
-
-# Change name of Maarianhamina
-   dat_muuttotiedot_kunnittain$Alue[dat_muuttotiedot_kunnittain$Alue == "Maarianhamina - Mariehamn"] <- "Maarianhamina"
-
-# Add seutukunta, maakunta and kuntaryhma to the data
-  dat_muuttotiedot_kunnittain <- dat_muuttotiedot_kunnittain %>%
-     filter(Alue != "KOKO MAA") %>%
-                        select(-Ikä, -Sukupuoli) %>%
-                        mutate(seutukunta = kuntaluokka(Alue, "Seutukunta")) %>%
-                        mutate(maakunta = kuntaluokka(Alue, "Maakunta")) %>%
-                        mutate(kuntaryhma = kuntaluokka(Alue, "Kuntaryhma"))
+devtools::load_all()
 
 # Load aluetyypit and väkilukutiedot
-   load("data/aluetyyppi.rda")
-   load("data/dat_kuntien_vakiluvut.rda")
+load("data/aluetyyppi.rda")
+load("data/dat_kuntien_vakiluvut.rda")
 
-# Add aluetyypit to the data
-   dat_muuttotiedot_kunnittain <- dat_muuttotiedot_kunnittain %>%
-                         rename(kunta = Alue) %>%
-                         left_join(aluetyyppi, by = "kunta")
 
-# Add väkilukutiedot to the data
-   dat_muuttotiedot_kunnittain <- dat_muuttotiedot_kunnittain %>%
-     rename(alue = kunta) %>%
-     left_join(dat_kuntien_vakiluvut, by = c("alue", "Vuosi"))
+# Get the data
+dat_muuttotiedot_kunnittain0 <-
+   get_pxweb_data(
+      url = "http://pxnet2.stat.fi/PXWeb/api/v1/fi/StatFin/vrm/muutl/statfin_muutl_pxt_11a2.px",
+      dims = list(
+         Alue = c('*'),
+         Vuosi = c('*'),
+         Sukupuoli = c('SSS'),
+         Ikä  = c('SSS'),
+         Tiedot = c('*')
+      ),
+      clean = TRUE
+   )
 
-# Compute muuttoasteet
-   dat_muuttotiedot_kunnittain <- dat_muuttotiedot_kunnittain %>%
-                         spread(Tiedot, values) %>%
-                         mutate(tulomuuttoaste = tulomuutto / vakiluku,
-                                lahtomuuttoaste = lahtomuutto / vakiluku,
-                                nettomuuttoaste = nettomuutto / vakiluku) %>%
-                         gather(Tiedot, values, c("tulomuutto", "lahtomuutto", "nettomuutto",
-                                                  "tulomuuttoaste", "lahtomuuttoaste", "nettomuuttoaste"))
+# Change types of Alue and Vuosi and names in Tiedot
+dat_muuttotiedot_kunnittain <- dat_muuttotiedot_kunnittain0 %>%
+   mutate(
+      Alue = as.character(Alue),
+      Vuosi = as.integer(as.character(Vuosi)),
+      Tiedot = fct_recode(Tiedot,
+         tulomuutto = "Kuntien välinen tulomuutto",
+         lahtomuutto = "Kuntien välinen lähtömuutto",
+         nettomuutto = "Kuntien välinen nettomuutto")) %>%
+   filter(Alue != "KOKO MAA") %>%
+   select(-Ikä, -Sukupuoli) %>%
+   rename(kunta = Alue) %>%
+   left_join(aluetyyppi, by = "kunta") %>%
+   rename(alue = kunta) %>%
+   left_join(dat_kuntien_vakiluvut, by = c("alue", "Vuosi")) %>%
+   mutate(alue = fct_recode(alue, Maarianhamina = "Maarianhamina - Mariehamn")) %>%
+   mutate(seutukunta = kuntaluokka(alue, "Seutukunta")) %>%
+   mutate(maakunta = kuntaluokka(alue, "Maakunta")) %>%
+   mutate(kuntaryhma = kuntaluokka(alue, "Kuntaryhma")) %>%
+   spread(Tiedot, values) %>%
+   mutate(
+      tulomuuttoaste = tulomuutto / vakiluku,
+      lahtomuuttoaste = lahtomuutto / vakiluku,
+      nettomuuttoaste = nettomuutto / vakiluku
+   ) %>%
+   gather(Tiedot, values, c("tulomuutto", "lahtomuutto", "nettomuutto",
+                            "tulomuuttoaste", "lahtomuuttoaste", "nettomuuttoaste")
+   )
+
 
 # Save data
    usethis::use_data(dat_muuttotiedot_kunnittain, overwrite = TRUE)
